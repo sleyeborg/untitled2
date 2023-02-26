b0a344602b6ce5c30b7871974ff9f19b6e2be368
sleyeborg <59716958+sleyeborg@users.noreply.github.com>
2023-02-25 18:38:05 -0800

    we set up a really solid web pack bundler and configured out package.json really well. we then made a threejs app in it.

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,38 +1,74 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-// init
+let camera, scene, renderer, controls, cube;
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-camera.position.z = 1;
+init();
+animate();
 
-const scene = new THREE.Scene();
+function init() {
+    // Renderer
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.xr.enabled = true;
+    document.body.appendChild( VRButton.createButton( renderer ) );
+    document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
+    // Scene
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x808080 );
 
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
+    // Camera
+    camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 10 );
+    camera.position.set( 0, 1.6, 3 );
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animation );
-document.body.appendChild( renderer.domElement );
-
-// orbit controls
-
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.enableDamping = true;
-
-// animation
-
-function animation( time ) {
-
-    mesh.rotation.x = time / 2000;
-    mesh.rotation.y = time / 1000;
-
+    // Controls
+    controls = new OrbitControls( camera, renderer.domElement );
+    controls.target.set( 0, 1.6, 0 );
     controls.update();
 
-    renderer.render( scene, camera );
+    // Cube
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+    cube = new THREE.Mesh( geometry, material );
+    cube.position.y = 1;
+    scene.add( cube );
 
+    // Lights
+    const light = new THREE.PointLight();
+    light.position.set( 0, 2, 2 );
+    camera.add( light );
+    scene.add( camera );
+
+    // Event listeners
+    window.addEventListener( 'resize', onWindowResize );
+
+    // XR
+    renderer.xr.addEventListener( 'sessionstart', onSessionStart );
+    renderer.xr.addEventListener( 'sessionend', onSessionEnd );
+}
+
+function animate() {
+    renderer.setAnimationLoop( render );
+}
+
+function render() {
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function onSessionStart() {
+    controls.enabled = false;
+}
+
+function onSessionEnd() {
+    controls.enabled = true;
 }
