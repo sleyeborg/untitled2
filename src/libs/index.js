00019ede989ab54ec26adbc1ea51dd1a1f117d15
sleyeborg <59716958+sleyeborg@users.noreply.github.com>
2023-02-25 20:09:07 -0800

    we set up a really solid web pack bundler and configured out package.json really well. we then made a threejs app in it. now we are messing in a threedee env. we have not been able to do webvr yet because we do not have a headset?

--- src/libs/index.js
+++ src/libs/index.js
@@ -21,25 +21,20 @@ const controls = new OrbitControls( camera, renderer.domElement );
 controls.target.set( 0, 1.6, 0 );
 controls.update();
 
-// Cube
+/*// Cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
 const cube = new THREE.Mesh( geometry, material );
-cube.position.y = 1;
+cube.position.y = 10;
 scene.add( cube );
 
 // White Cube
 const whiteCubeGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
 const whiteCubeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
 const whiteCube = new THREE.Mesh(whiteCubeGeometry, whiteCubeMaterial);
-whiteCube.position.y = 1.5;
+whiteCube.position.y = 15;
 cube.add(whiteCube);
-
-// Lights
-const light = new THREE.PointLight();
-light.position.set( 0, 2, 2 );
-camera.add( light );
-scene.add( camera );
+*/
 
 // Event listeners
 window.addEventListener( 'resize', onWindowResize );
@@ -54,7 +49,7 @@ function animate() {
 }
 
 function render() {
-    cube.rotation.y += 0.01;
+    //cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 }
 
@@ -77,22 +72,22 @@ function onDocumentKeyDown( event ) {
     const speed = 0.1;
     switch (keyCode) {
         case 'ArrowUp':
-            whiteCube.position.z -= speed;
+            camera.position.z -= speed;
             break;
         case 'ArrowDown':
-            whiteCube.position.z += speed;
+            camera.position.z += speed;
             break;
         case 'ArrowLeft':
-            whiteCube.position.x -= speed;
+            camera.position.x -= speed;
             break;
         case 'ArrowRight':
-            whiteCube.position.x += speed;
+            camera.position.x += speed;
             break;
         case 'KeyW':
-            whiteCube.position.y += speed;
+            camera.position.y += speed;
             break;
         case 'KeyS':
-            whiteCube.position.y -= speed;
+            camera.position.y -= speed;
             break;
     }
 }
