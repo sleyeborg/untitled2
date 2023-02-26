cd0461ccf327c3c53d0a7cd6cd16e7dc50cc9468
sleyeborg <59716958+sleyeborg@users.noreply.github.com>
2023-02-25 20:21:16 -0800

    we set up a really solid web pack bundler and configured out package.json really well. we then made a threejs app in it. now we are messing in a threedee env. we have not been able to do webvr yet because we do not have a headset?

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,32 +1,33 @@
-import * as THREE from 'three';
-import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { createScene } from '../controllers.js';
+import * as THREE from "three";
+import { VRButton } from "three/examples/jsm/webxr/VRButton.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { createScene } from "../scene.js";
+import { handleKeyDown } from "../controls.js";
 
 const scene = createScene();
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.xr.enabled = true;
-document.body.appendChild( VRButton.createButton( renderer ) );
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(VRButton.createButton(renderer));
+document.body.appendChild(renderer.domElement);
 
 // Camera
-const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 10 );
-camera.position.set( 0, 1.6, 3 );
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10);
+camera.position.set(0, 1.6, 3);
 
 // Controls
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.target.set( 0, 1.6, 0 );
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 1.6, 0);
 controls.update();
 
 /*// Cube
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
-const cube = new THREE.Mesh( geometry, material );
+const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
 cube.position.y = 10;
-scene.add( cube );
+scene.add(cube);
 
 // White Cube
 const whiteCubeGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
@@ -37,26 +38,26 @@ cube.add(whiteCube);
 */
 
 // Event listeners
-window.addEventListener( 'resize', onWindowResize );
-document.addEventListener( 'keydown', onDocumentKeyDown );
+window.addEventListener("resize", onWindowResize);
+document.addEventListener("keydown", (event) => handleKeyDown(event, camera, 0.1));
 
 // XR
-renderer.xr.addEventListener( 'sessionstart', onSessionStart );
-renderer.xr.addEventListener( 'sessionend', onSessionEnd );
+renderer.xr.addEventListener("sessionstart", onSessionStart);
+renderer.xr.addEventListener("sessionend", onSessionEnd);
 
 function animate() {
-    renderer.setAnimationLoop( render );
+    renderer.setAnimationLoop(render);
 }
 
 function render() {
     //cube.rotation.y += 0.01;
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth-100, window.innerHeight-100);
 }
 
 function onSessionStart() {
@@ -65,31 +66,6 @@ function onSessionStart() {
 
 function onSessionEnd() {
     controls.enabled = true;
-}
-
-function onDocumentKeyDown( event ) {
-    const keyCode = event.code;
-    const speed = 0.1;
-    switch (keyCode) {
-        case 'ArrowUp':
-            camera.position.z -= speed;
-            break;
-        case 'ArrowDown':
-            camera.position.z += speed;
-            break;
-        case 'ArrowLeft':
-            camera.position.x -= speed;
-            break;
-        case 'ArrowRight':
-            camera.position.x += speed;
-            break;
-        case 'KeyW':
-            camera.position.y += speed;
-            break;
-        case 'KeyS':
-            camera.position.y -= speed;
-            break;
-    }
 }
 
 animate();
