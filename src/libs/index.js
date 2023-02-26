428b5e6af559228dbbabd2f641e185f20f41d65b
sleyeborg <59716958+sleyeborg@users.noreply.github.com>
2023-02-25 20:30:52 -0800

    we set up a really solid web pack bundler and configured out package.json really well. we then made a threejs app in it. now we are messing in a threedee env. we have not been able to do webvr yet because we do not have a headset?

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,10 +1,11 @@
-import * as THREE from "three";
-import { VRButton } from "three/examples/jsm/webxr/VRButton.js";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { createScene } from "../scene.js";
-import { handleKeyDown } from "../controls.js";
+import * as THREE from 'three';
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { createScene } from '../scene.js';
+import { createCamera } from '../camera.js';
+import { handleKeyDown } from '../controls.js';
 
-const scene = createScene();
+const { scene, camera } = createScene();
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -13,51 +14,31 @@ renderer.xr.enabled = true;
 document.body.appendChild(VRButton.createButton(renderer));
 document.body.appendChild(renderer.domElement);
 
-// Camera
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 10);
-camera.position.set(0, 1.6, 3);
-
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 1.6, 0);
 controls.update();
 
-/*// Cube
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
-const cube = new THREE.Mesh(geometry, material);
-cube.position.y = 10;
-scene.add(cube);
-
-// White Cube
-const whiteCubeGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-const whiteCubeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-const whiteCube = new THREE.Mesh(whiteCubeGeometry, whiteCubeMaterial);
-whiteCube.position.y = 15;
-cube.add(whiteCube);
-*/
-
 // Event listeners
-window.addEventListener("resize", onWindowResize);
-document.addEventListener("keydown", (event) => handleKeyDown(event, camera, 0.1));
+window.addEventListener('resize', onWindowResize);
+document.addEventListener('keydown', (event) => handleKeyDown(event, camera, 0.1));
 
 // XR
-renderer.xr.addEventListener("sessionstart", onSessionStart);
-renderer.xr.addEventListener("sessionend", onSessionEnd);
+renderer.xr.addEventListener('sessionstart', onSessionStart);
+renderer.xr.addEventListener('sessionend', onSessionEnd);
 
 function animate() {
     renderer.setAnimationLoop(render);
 }
 
 function render() {
-    //cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth-100, window.innerHeight-100);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function onSessionStart() {
