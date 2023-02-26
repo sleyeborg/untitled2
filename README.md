fb3299a0e16e9e8a59c3397cc3243eb52922127a
sleyeborg <59716958+sleyeborg@users.noreply.github.com>
2023-02-25 16:34:59 -0800

    we set up a really solid web pack bundler and configured out package.json really well. we then made a threejs app in it.

---
+++ README.md
@@ -0,0 +1,9 @@
+in this project, if you want to call webpack commands, 
+you must use this kind of command
+"
+./node_modules/.bin/webpack ./src/index.js --output-path ./dist/ --output-filename bundle.js --mode development   
+"
+the call was =>./node_modules/.bin/webpack -----the rest are options. 
+*****************************
+*./node_modules/.bin/webpack*
+*****************************
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module.exports = {
+    mode: "development",
+    entry: './src/libs/index.js',
+    output: {
+        filename: '[name].[chunk-hash].js',
+        path: path.resolve(__dirname, 'dist')
+
+    },
+    plugins: [
+        new CleanWebpackPlugin({cleanOnceBeforeBuildPatterns: ['*.js']}),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            hash : 'true'
+        })
+
+    ]
+
+
+}
