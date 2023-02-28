7b915d5b0ed34a6efaaf63a3daf64873f001c365
sleyeborg <59716958+sleyeborg@users.noreply.github.com>
2023-02-27 19:23:47 -0800

    we set up a really solid web pack bundler and configured out package.json really well. we then made a threejs app in it. now we are messing in a threedee env. we have not been able to do webvr yet because we do not have a headset?

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,52 +1,130 @@
 import * as THREE from 'three';
-import { VRButton } from 'three/examples/jsm/webxr/VRButton.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { createScene } from '../scene.js';
-import { createCamera } from '../camera.js';
-import { handleKeyDown } from '../controls.js';
-
-const { scene, camera } = createScene();
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.xr.enabled = true;
-document.body.appendChild(VRButton.createButton(renderer));
 document.body.appendChild(renderer.domElement);
 
-// Controls
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 1.6, 0);
-controls.update();
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 2);
 
-// Event listeners
-window.addEventListener('resize', onWindowResize);
-document.addEventListener('keydown', (event) => handleKeyDown(event, camera, 0.1));
+const planetGeometry = new THREE.SphereGeometry(1, 32, 32);
+const planetMaterial = new THREE.MeshBasicMaterial({ color: '#aa11ff' });
+const planet = new THREE.Mesh(planetGeometry, planetMaterial);
 
-// XR
-renderer.xr.addEventListener('sessionstart', onSessionStart);
-renderer.xr.addEventListener('sessionend', onSessionEnd);
+const meshGeometry = new THREE.SphereGeometry(50, 32, 32);
+const meshMaterial = new THREE.MeshBasicMaterial({ color: '#5ada95' });
+const mesh = new THREE.Mesh(meshGeometry, meshMaterial);
 
-function animate() {
-    renderer.setAnimationLoop(render);
+planet.add(mesh);
+
+planet.position.set(10, 10, 10);
+camera.lookAt(10, 10, 10);
+const scene = new THREE.Scene();
+
+// Add a skybox
+// Define the vertex shader
+const skyVertexShader = `
+varying vec2 vUv;
+void main() {
+  vUv = uv;
+  vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+  gl_Position = projectionMatrix * modelViewPosition;
+}`;
+
+// Define the fragment shader
+// Define the fragment shader
+const skyFragmentShader = `
+  varying vec2 vUv;
+  uniform sampler2D texture1;
+  void main() {
+    // Get the pixel value from the texture using the UV coordinates
+    vec4 pixel = texture2D(texture1, vUv);
+
+    // Convert the pixel value to grayscale
+    float grayscale = dot(pixel.rgb, vec3(0.299, 0.587, 0.114));
+
+    // Set the final color of the fragment
+    gl_FragColor = vec4(vec3(grayscale), 1.0);
+  }
+`;
+
+// Define the SVG matrix as a 3x3 array
+const svgMatrix = [  [1, 0, 1],
+    [0, 1, 0],
+    [1, 0, 1]
+];
+
+// Create a canvas and context for generating the SVG texture
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
+canvas.width = canvas.height = 3;
+const imageData = context.createImageData(3, 3);
+
+// Loop through the SVG matrix and set the pixel data
+for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 3; x++) {
+        const value = svgMatrix[y][x] ? 255 : 0;
+        const index = (y * 3 + x) * 4;
+        imageData.data[index] = value;
+        imageData.data[index + 1] = value;
+        imageData.data[index + 2] = value;
+        imageData.data[index + 3] = 255;
+    }
 }
+
+// Create the texture and set the pixel data
+const texture = new THREE.CanvasTexture(canvas);
+texture.needsUpdate = true;
+texture.image = imageData;
+
+// Create the shader material
+const skyMaterial = new THREE.ShaderMaterial({
+    vertexShader: skyVertexShader,
+    fragmentShader: skyFragmentShader,
+    side: THREE.BackSide,
+    uniforms: {
+        texture1: { value: texture }
+    }
+});
+
+// Create the skybox geometry
+const skyGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
+
+// Create the skybox mesh
+const sky = new THREE.Mesh(skyGeometry, skyMaterial);
+scene.add(planet);
+
+// Add the skybox to the scene
+scene.add(sky);
+
+// Add a floor plane
+const groundGeometry = new THREE.PlaneGeometry(10, 10, 1, 1);
+const groundMaterial = new THREE.MeshBasicMaterial({ color: '#5E5E5E' }); // Gray color
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.rotation.x = -Math.PI / 2; // Rotate to lie flat on the ground
+scene.add(ground);
+
+// Add a blue point light
+const pointLight1 = new THREE.PointLight(0x12BFaF, 0,3, 100);
+pointLight1.position.set(10, 5, 400);
+scene.add(pointLight1);
+
+// Add a purple directional light
+const dirLight = new THREE.DirectionalLight(0xaa99bF, 0.2);
+dirLight.position.set(0, 100, 1);
+scene.add(dirLight);
+
+const clock = new THREE.Clock();
 
 function render() {
     renderer.render(scene, camera);
 }
 
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+function animate() {
+    requestAnimationFrame(animate); // Call this function again on the next frame
+   // const elapsedTime = clock.getElapsedTime(); // Get the time elapsed since the last frame
+    //planet.rotation.y = elapsedTime +=1  ; // Rotate the planet around the y-axis
+    //mesh.rotation.y = elapsedTime *3.1; // Rotate the mesh around the y-axis
+    renderer.render(scene, camera);
 }
-
-function onSessionStart() {
-    controls.enabled = false;
-}
-
-function onSessionEnd() {
-    controls.enabled = true;
-}
-
 animate();
