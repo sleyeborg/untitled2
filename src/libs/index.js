70d9b978a203b1bc9bdcb8999aa25599787f9cce
sleyeborg <59716958+sleyeborg@users.noreply.github.com>
2023-02-27 21:00:21 -0800

    solid webpack and package.json. good file structure. controls enabled. almost no dependencies. very well made

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,6 +1,12 @@
 import * as THREE from 'three';
 import { setupKeyControls, getKeys } from '../controls.js';
+import {svgMatrix} from '../shaders.js'
 
+// Create a canvas and context for generating the SVG texture
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
+canvas.width = canvas.height = 3;
+const imageData = context.createImageData(3, 3);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -14,13 +20,13 @@ const planetMaterial = new THREE.MeshBasicMaterial({ color: '#aa11ff' });
 const planet = new THREE.Mesh(planetGeometry, planetMaterial);
 
 const meshGeometry = new THREE.SphereGeometry(50, 32, 32);
-const meshMaterial = new THREE.MeshBasicMaterial({ color: '#5ada95',wireframe:true });
+const meshMaterial = new THREE.MeshBasicMaterial({ color: '#5ada95', wireframe: true });
 const mesh = new THREE.Mesh(meshGeometry, meshMaterial);
 
 planet.add(mesh);
 
-planet.position.set(0, 0, 0);
-camera.lookAt(0, 0, 0);
+planet.position.set(3, 3, 3);
+camera.lookAt(3, 3, 3);
 const scene = new THREE.Scene();
 
 // Add a skybox
@@ -34,45 +40,19 @@ void main() {
 }`;
 
 // Define the fragment shader
-// Define the fragment shader
 const skyFragmentShader = `
-  varying vec2 vUv;
-  uniform sampler2D texture1;
-  void main() {
-    // Get the pixel value from the texture using the UV coordinates
-    vec4 pixel = texture2D(texture1, vUv);
+varying vec2 vUv;
+uniform sampler2D texture1;
+void main() {
+  // Get the pixel value from the texture using the UV coordinates
+  vec4 pixel = texture2D(texture1, vUv);
 
-    // Convert the pixel value to grayscale
-    float grayscale = dot(pixel.rgb, vec3(0.299, 0.587, 0.114));
+  // Convert the pixel value to grayscale
+  float grayscale = dot(pixel.rgb, vec3(0.299, 0.587, 0.114));
 
-    // Set the final color of the fragment
-    gl_FragColor = vec4(vec3(grayscale), 1.0);
-  }
-`;
-
-// Define the SVG matrix as a 3x3 array
-const svgMatrix = [  [1, 0, 1],
-    [0, 1, 0],
-    [1, 0, 1]
-];
-
-// Create a canvas and context for generating the SVG texture
-const canvas = document.createElement('canvas');
-const context = canvas.getContext('2d');
-canvas.width = canvas.height = 3;
-const imageData = context.createImageData(3, 3);
-
-// Loop through the SVG matrix and set the pixel data
-for (let y = 0; y < 3; y++) {
-    for (let x = 0; x < 3; x++) {
-        const value = svgMatrix[y][x] ? 255 : 0;
-        const index = (y * 3 + x) * 4;
-        imageData.data[index] = value;
-        imageData.data[index + 1] = value;
-        imageData.data[index + 2] = value;
-        imageData.data[index + 3] = 255;
-    }
-}
+  // Set the final color of the fragment
+  gl_FragColor = vec4(vec3(grayscale), 1.0);
+}`;
 
 // Create the texture and set the pixel data
 const texture = new THREE.CanvasTexture(canvas);
@@ -85,8 +65,8 @@ const skyMaterial = new THREE.ShaderMaterial({
     fragmentShader: skyFragmentShader,
     side: THREE.BackSide,
     uniforms: {
-        texture1: { value: texture }
-    }
+        texture1: { value: texture },
+    },
 });
 
 // Create the skybox geometry
@@ -107,13 +87,17 @@ ground.rotation.x = -Math.PI / 2; // Rotate to lie flat on the ground
 scene.add(ground);
 
 // Add a blue point light
-const pointLight1 = new THREE.PointLight(0x12BFaF, 0,3, 100);
-pointLight1.position.set(10, 5, 400);
+const pointLight1 = new THREE.PointLight(0x12BFaF, 1, 100);
+//pointLight
+
+// Add a blue point light
+//const pointLight1 = new THREE.PointLight(0x12BFaF, 0,6, 100);
+pointLight1.position.set(3, 3, 3);
 scene.add(pointLight1);
 
 // Add a purple directional light
 const dirLight = new THREE.DirectionalLight(0xaa99bF, 0.2);
-dirLight.position.set(0, 100, 1);
+dirLight.position.set(3, 3, 3);
 scene.add(dirLight);
 
 const clock = new THREE.Clock();
@@ -126,15 +110,18 @@ function getElapsedTime() {
 }
 setupKeyControls();
 function animate() {
-    let keyBuffer = getKeys();
+    const keyBuffer = getKeys();
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    svgMatrix(imageData);
+    context.putImageData(imageData, 0, 0);
 
 
     const elapsedTime = clock.getElapsedTime(); // Get the time elapsed since the last frame
-    //planet.rotation.y = elapsedTime *=1  ; // Rotate the planet around the y-axis
     mesh.rotation.y = elapsedTime *0.3; // Rotate the mesh around the y-axis
     const speed = 0.01;
     if (keyBuffer.includes(37)) { // left arrow
         planet.rotation.y += speed;
+
     }
     if (keyBuffer.includes(38)) { // up arrow
         planet.rotation.x += speed;
