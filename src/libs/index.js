16facd6e5fc4d00a3388d8efd5f646bc476e6d10
sleyeborg <59716958+sleyeborg@users.noreply.github.com>
2023-02-27 21:00:21 -0800

    solid webpack and package.json. good file structure. controls enabled. almost no dependencies. very well made lol. ok. tricolor physics engine

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,163 +1,33 @@
-/**********
-* imports *
-**********/
 import * as THREE from 'three';
-import {createCamera} from "./components/camera";
-import {createScene} from "./components/scene";
-import {createLights} from "./components/lights";
-import { getCntrlflags, onKeyUp, onKeyDown } from './controls.js';
-import { handleKeyDown, handleKeyUp } from "../eventHandlers.js";
-import {createRenderer} from "./systems/renderer";
-import {skymake} from "./components/worlds/skymake";
-import {groundmake} from "./components/worlds/groundmake";
-import {Planet} from "./components/worlds/planetmake";
-import {UniversalScopeWorker}  from "./components/worlds/physics/UniversalScopeWorker";
+import { setupKeyControls, getKeys } from '../controls.js';
+import {svgMatrix} from '../shaders.js'
 
-
-/******************************************************
-* initialize canvas and add event listeners.dom stuff *
-******************************************************/
-
+// Create a canvas and context for generating the SVG texture
 const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
 canvas.width = canvas.height = 3;
+const imageData = context.createImageData(3, 3);
 
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-document.addEventListener('keydown', onKeyDown);
-document.addEventListener('keyup', onKeyUp);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 2);
 
-/******************************************************
- * render pipeline?                                   *
- *****************************************************/
-//initialize
-const renderer = createRenderer();
-const camera = createCamera();
-const scene = createScene();
-const planet = new Planet();
-planet.changemass(5000);
-const sky = skymake();
-const planet2 = new Planet();
-planet2.changemass(4000);
-const planet3 = new Planet();
-planet3.changemass(3000);
-const pointLight1 = createLights();
-const dirLight = createLights();
-//populate
-scene.add(planet3);
-scene.add(planet2);
-scene.add(planet);
-scene.add(sky);
-scene.add(dirLight);
-scene.add(pointLight1);
-//manipulate
-camera.position.set(0, 40, -40);
-planet.position.set(1, 1, 5);
-planet2.rotation.x = -Math.PI / 2; // Rotate to lie flat on the ground
-planet2.position.set(3,3,3);
-planet3.position.set(4,4,4);
+const planetGeometry = new THREE.SphereGeometry(1, 32, 32);
+const planetMaterial = new THREE.MeshBasicMaterial({ color: '#aa11ff' });
+const planet = new THREE.Mesh(planetGeometry, planetMaterial);
 
-planet.changemass(5000);
+const meshGeometry = new THREE.SphereGeometry(50, 32, 32);
+const meshMaterial = new THREE.MeshBasicMaterial({ color: '#5ada95', wireframe: true });
+const mesh = new THREE.Mesh(meshGeometry, meshMaterial);
 
+planet.add(mesh);
 
-
-
-const uniworker = new UniversalScopeWorker();
-uniworker.getScene(scene);
-const meshes = uniworker.getMeshes()
-//const meshes = uniworker.getMeshes();
-//planet2.checkPlanetDirectory(uniworker);
-//console.log("ASDasasP"+JSON.stringify(planet2.planetRegistry));
-
-//planet3.checkPlanetDirectory(uniworker);
-//console.log("33P3"+planet3.planetRegistry);
-//uniworker.removePlanetFromDirectory(planet);
-
-//uniworker.addPlanetToDirectory(planet);
-//uniworker.addPlanetToDirectory(planet2);
-//console.log("PlanetDirectory:", JSON.stringify(uniworker.planetDirectory));
-//planet.checkPlanetDirectory(uniworker);
-//console.log("ASDP"+JSON.stringify(planet.planetRegistry));
-//console.log("asdfaa"+JSON.stringify(planet2.planetRegistry));
-//console.log("a"+JSON.stringify(planet3.planetRegistry));
-
-
-
-
-pointLight1.position.set(10, 5, 400);
-dirLight.position.set(0, 100, 1);
-camera.lookAt(0, 2, 5);
-
-const clock = new THREE.Clock();
-function render() {
-    renderer.render(scene, camera);
-}
-
-function getCurrentTime() {
-    return performance.now() - clock.start();
-}
-//setupKeyControls();
-//const keyCombos = getKeys();
-
-const fps = 54; // target frame rate
-const interval = 1000 / fps; // time interval in ms per frame
-
-let previousTime = 0;
-let lag = 0;
-
-function animate() {
-
-    requestAnimationFrame(animate); // Call this function again on the next frame
-
-
-    let cf = getCntrlflags();
-    const elapsedTime = clock.getElapsedTime(); // Get the time elapsed since the last frame
-    const currentTime = getCurrentTime();
-    const elapsed = currentTime - previousTime;
-    previousTime = currentTime;
-    lag += elapsed;
-    update();
-
-    const buffer = [];
-    buffer.push(cf);
-
-    //planet.rotation.y = elapsedTime *=1  ; // Rotate the planet around the y-axis
-    planet.rotation.y = elapsedTime * 0.2; // Rotate the mesh around the y-axis
-    //const speed = 0.01;
-    document.addEventListener("keydown", (event) =>
-        handleKeyDown(event, camera)
-    );
-    document.addEventListener("keyup", handleKeyUp);
-
-
-
-    renderer.render(scene, camera);
-
-}
-
-function update(){
-    //return an object with all the planets in the  scene
-    const mesh = uniworker.getMeshes(scene);
-
-   const planetList = scene.children.filter((child) => child instanceof Planet);
-  // planetList.forEach((planet)=>{uniworker.addPlanetToDirectory(planet);console.log("UNIWORKERADD"+JSON.stringify(planet.planetRegistry))});
-   //planetList.forEach((planet) => {planet.checkPlanetDirectory(uniworker);console.log("PLANETCHECK"+JSON.stringify(planet.planetRegistry))});
-    planetList.forEach((planet)=>{planet.update(uniworker)});
-   // planetList.forEach((planet)=>{uniworker.removePlanetFromDirectory(planet);console.log("UNIWORKERREMOVE"+JSON.stringify(planet.planetRegistry))});
-
-   //nonono planetList.forEach((planet)=>{planet.update(uniworker)});
-
-}
-
-
-
-
-animate();
-
-
-
-
-
-/* //this is a whole bunch of chat chpt generated SVG stuff
-//const imageData = context.createImageData(3, 3);
+planet.position.set(3, 3, 3);
+camera.lookAt(3, 3, 3);
+const scene = new THREE.Scene();
 
 // Add a skybox
 // Define the vertex shader
@@ -170,43 +40,24 @@ void main() {
 }`;
 
 // Define the fragment shader
-// Define the fragment shader
 const skyFragmentShader = `
-  varying vec2 vUv;
-  uniform sampler2D texture1;
-  void main() {
-    // Get the pixel value from the texture using the UV coordinates
-    vec4 pixel = texture2D(texture1, vUv);
+varying vec2 vUv;
+uniform sampler2D texture1;
+void main() {
+  // Get the pixel value from the texture using the UV coordinates
+  vec4 pixel = texture2D(texture1, vUv);
 
-    // Convert the pixel value to grayscale
-    float grayscale = dot(pixel.rgb, vec3(0.299, 0.587, 0.114));
+  // Convert the pixel value to grayscale
+  float grayscale = dot(pixel.rgb, vec3(0.299, 0.587, 0.114));
 
-    // Set the final color of the fragment
-    gl_FragColor = vec4(vec3(grayscale), 1.0);
-  }
+  // Set the final color of the fragment
+  gl_FragColor = vec4(vec3(grayscale), 1.0);
+}`;
 
-
-// Define the SVG matrix as a 3x3 array
-const svgMatrix = [  [1, 0, 1],
-    [0, 1, 0],
-    [1, 0, 1]
-];
-// Create a canvas and context for generating the SVG texture
-// Loop through the SVG matrix and set the pixel data
-for (let y = 0; y < 3; y++) {
-    for (let x = 0; x < 3; x++) {
-        const value = svgMatrix[y][x] ? 255 : 0;
-        const index = (y * 3 + x) * 4;
-        imageData.data[index] = value;
-        imageData.data[index + 1] = value;
-        imageData.data[index + 2] = value;
-        imageData.data[index + 3] = 255;
-    }
-}
 // Create the texture and set the pixel data
-//const texture = new THREE.CanvasTexture(canvas);
-//texture.needsUpdate = true;
-//texture.image = imageData;
+const texture = new THREE.CanvasTexture(canvas);
+texture.needsUpdate = true;
+texture.image = imageData;
 
 // Create the shader material
 const skyMaterial = new THREE.ShaderMaterial({
@@ -214,7 +65,77 @@ const skyMaterial = new THREE.ShaderMaterial({
     fragmentShader: skyFragmentShader,
     side: THREE.BackSide,
     uniforms: {
-        texture1: { value: texture }
-    }
+        texture1: { value: texture },
+    },
 });
-*/
+
+// Create the skybox geometry
+const skyGeometry = new THREE.BoxGeometry(1000, 1000, 1000);
+
+// Create the skybox mesh
+const sky = new THREE.Mesh(skyGeometry, skyMaterial);
+scene.add(planet);
+
+// Add the skybox to the scene
+scene.add(sky);
+
+// Add a floor plane
+const groundGeometry = new THREE.PlaneGeometry(10, 10, 1, 1);
+const groundMaterial = new THREE.MeshBasicMaterial({ color: '#5E5E5E' }); // Gray color
+const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+ground.rotation.x = -Math.PI / 2; // Rotate to lie flat on the ground
+scene.add(ground);
+
+// Add a blue point light
+const pointLight1 = new THREE.PointLight(0x12BFaF, 1, 100);
+//pointLight
+
+// Add a blue point light
+//const pointLight1 = new THREE.PointLight(0x12BFaF, 0,6, 100);
+pointLight1.position.set(3, 3, 3);
+scene.add(pointLight1);
+
+// Add a purple directional light
+const dirLight = new THREE.DirectionalLight(0xaa99bF, 0.2);
+dirLight.position.set(3, 3, 3);
+scene.add(dirLight);
+
+const clock = new THREE.Clock();
+
+function render() {
+    renderer.render(scene, camera);
+}
+function getElapsedTime() {
+    return clock.getElapsedTime();
+}
+setupKeyControls();
+function animate() {
+    const keyBuffer = getKeys();
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    svgMatrix(imageData);
+    context.putImageData(imageData, 0, 0);
+
+
+    const elapsedTime = clock.getElapsedTime(); // Get the time elapsed since the last frame
+    mesh.rotation.y = elapsedTime *0.3; // Rotate the mesh around the y-axis
+    const speed = 0.01;
+    if (keyBuffer.includes(37)) { // left arrow
+        planet.rotation.y += speed;
+
+    }
+    if (keyBuffer.includes(38)) { // up arrow
+        planet.rotation.x += speed;
+    }
+    if (keyBuffer.includes(39)) { // right arrow
+        planet.rotation.y -= speed;
+    }
+    if (keyBuffer.includes(40)) { // down arrow
+        planet.rotation.x -= speed;
+    }
+    //const keys = getKeys();
+    //console.log(keys);
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate); // Call this function again on the next frame
+
+}
+animate();
